d95e266e7ea45dba7cd5fb106716ba1a079fb111
izh5076 <izh5076@psu.edu>
2022-04-01 13:54:56 -0400

    Initial Commit

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public abstract class Person
+{
+    //instance variables
+    private String name;
+    private String gender;
+
+
+    public Person()
+    {
+        this.name = null;
+        this.gender = null;
+    }
+
+    /**
+     * Two parameter constructor to set the name and gender of the person.
+     * @param name1 name of the person
+     * @param gender1 gender of the person
+     */
+    public Person(String name1, String gender1)
+    {
+        this.name = name1;
+        this.gender = gender1;
+    }
+
+}
