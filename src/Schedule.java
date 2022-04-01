a48007d4d914bee53407a24768e5e8e26fb15817
Christopher Brennen <crb6032@psu.edu>
2022-04-01 14:47:31 -0400

    schedule and timeslotexception

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 
 public class Schedule {
-    ArrayList<Date> schedule = new ArrayList<Date>();
+    ArrayList<Appointment> schedule = new ArrayList<Appointment>();
+
     public Schedule(){}
-    public void add( Date date ){
-        schedule.add( date );
+
+    public void add( Appointment apt ) throws TimeSlotFilledException{
+        for ( Appointment a : schedule ) {
+            if( apt.getDate().equals( a.getDate() ) && apt.getTime().equals( a.getTime() ) ){
+                throw new TimeSlotFilledException( apt.getDate(), apt.getTime() );
+            }
+        }
+
+        schedule.add( apt );
     }
+
     public void displaySchedule(){
         System.out.println("The schedule is as follows:");
-        for (Date d : schedule) {
-            System.out.println(d.toString());
+        for (Appointment a : schedule) {
+            System.out.println(a.toString());
         }
     }
 }
---
+++ src/TimeSlotFilledException.java
@@ -0,0 +1,18 @@
+public class TimeSlotFilledException extends Exception{
+    Date date;
+    int time;
+    public TimeSlotFilledException( Date date, int time ){
+        super("The time slot you are trying to schedule is already full: " + date.toString() + " " + time + ":00.");
+        this.date = date;
+        this.time = time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+}
