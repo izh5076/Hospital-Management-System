17b2563a8ab553ccd3ba3bee29e7cc50b447c351
Christopher Brennen <crb6032@psu.edu>
2022-04-18 20:24:13 -0400

    assing Hospital

---
+++ src/Hospital.java
@@ -0,0 +1,12 @@
+/**
+ * Hospital.java:
+ * this will hold the main and method logic
+ *
+ * @author Christopher Brennen
+ * @author Issac Heim
+ */
+public class Hospital {
+    public static void main(String[] args) {
+
+    }
+}
